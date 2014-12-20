a639cd1aede9fed00813de223560615b715c2ec7
yeahugo <yjhuestc@gmail.com>
2014-12-20 11:18:12 +0800

    project init

---
+++ AiToyMaker/UI/AiToyStoreViewController.swift
@@ -0,0 +1,50 @@
+//
+//  AiToyStoreViewController.swift
+//  AiToyMaker
+//
+//  Created by Gavin Ye on 12/18/14.
+//  Copyright (c) 2014 Aijingang. All rights reserved.
+//
+
+import UIKit
+
+class AiToyStoreViewController: UIViewController,UICollectionViewDataSource {
+    
+    let collectionCellIdentifier = "CollectionCell"
+    var data:[String] = []
+    @IBOutlet var collectionView:UICollectionView!
+    @IBOutlet var menuView:UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.collectionView.dataSource = self
+        self.collectionView.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: collectionCellIdentifier)
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(collectionCellIdentifier, forIndexPath: indexPath) as UICollectionViewCell
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
