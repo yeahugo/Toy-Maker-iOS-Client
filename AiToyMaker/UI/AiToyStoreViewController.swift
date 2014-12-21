b6c2ae1e8749d9ad2921adc1c21196156c635b43
yeahugo <yjhuestc@gmail.com>
2014-12-21 18:35:43 +0800

    get image from server

--- AiToyMaker/UI/AiToyStoreViewController.swift
+++ AiToyMaker/UI/AiToyStoreViewController.swift
@@ -7,27 +7,48 @@
 //
 
 import UIKit
-//import Alamofire
+import Alamofire
+
+enum ModelType{
+    case Cartoon,Pinzhuang
+}
 
 class AiToyStoreViewController: UIViewController,UICollectionViewDataSource,UICollectionViewDelegate {
     
     let collectionCellIdentifier = "CollectionCell"
     var images:[String]?
-    var selectIndex:Int?
+    var models:[NSDictionary] = []
+    var hostName:String = "http://192.168.1.103:8000/"
+    var modelType = ModelType.Cartoon
     
     @IBOutlet var collectionView:UICollectionView!
     @IBOutlet var menuView:UIView!
 
+    @IBAction func changeModelsFilter(sender: UISegmentedControl){
+        switch sender.selectedSegmentIndex {
+        case 0:
+            modelType = .Cartoon
+        case 1:
+            modelType = .Pinzhuang
+        default:
+            println("Bad segment index!")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.collectionView.dataSource = self
         images = ["a.png",  "b.png", "c.png", "d.png", "e.png", "f.png", "g.png", "h.png",  "j.png", "k.png", "l.png", "m.png"]
         
-//        Alamofire.request(.GET, "https://dl.dropboxusercontent.com/u/7817937/nameko.json")
-//        .responseJSON { (request, response, JSON, error) -> Void in
-////              self.images = JSON?.valueForKey("images") as? [String]
-//              NSLog("images is %@", self.images!)
-//        }
+//        AlamofireErrorDomain
+        var urlString = hostName + "models/catalog/5490cb83884bebe72f62acb8"
+        Alamofire.request(.GET, urlString, parameters: nil, encoding: .JSON)
+            .responseJSON { (request, response, JSON, error) -> Void in
+//                println( JSON)
+                self.models = JSON as [NSDictionary]
+                println(self.models)
+            self.collectionView.reloadData()
+       }
         // Do any additional setup after loading the view.
     }
 
@@ -37,40 +58,44 @@ class AiToyStoreViewController: UIViewController,UICollectionViewDataSource,UICo
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.images!.count
+        return self.models.count
+//        return self.images!.count
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         var cell:AiCollectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier(collectionCellIdentifier, forIndexPath: indexPath) as AiCollectionViewCell
-        let imageName = (self.images?[indexPath.row])?
-        cell.imageView.image = UIImage(named: imageName!)
-//        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(collectionCellIdentifier, forIndexPath: indexPath) as UICollectionViewCell
+        
+        println(self.models[indexPath.row].valueForKey("thumbnail"))
+        var thumbnailString:String = self.models[indexPath.row].valueForKey("thumbnail") as String
+        let range:Range = Range(start: advance(thumbnailString.startIndex, 1), end: advance(thumbnailString.endIndex, -1))
+        thumbnailString = thumbnailString.substringWithRange(range)
+        let thumbnailArray:Array = thumbnailString.componentsSeparatedByString(",")
+        var imageName = thumbnailArray.first!
+        imageName = imageName.substringWithRange(Range(start: advance(imageName.startIndex, 1), end: advance(imageName.endIndex,-1)))
+        let imageUrl = self.hostName + imageName
+        ImageLoader.sharedLoader.imageForUrl(imageUrl, completionHandler:{(image: UIImage?, url: String) in
+            cell.imageView.image = image
+        })
+        
+//        let imageName = (self.images?[indexPath.row])?
+//        cell.imageView.image = UIImage(named: imageName!)
         
         return cell
     }
     
     func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
-        NSLog("index path is %@", indexPath)
-//        selectIndex = indexPath.row
-//        collectionView.deselectItemAtIndexPath(indexPath, animated: true)
         let detailViewController:AiToyDetailViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("ToyDetail") as AiToyDetailViewController
         let imageName = self.images?[indexPath.row]
         detailViewController.portraitImageName = imageName
-//        let portraitImage:UIImage = UIImage(named: imageName!)!
-//        detailViewController.portraitImageView.image = portraitImage
         self.navigationController?.pushViewController(detailViewController, animated: true)
     }
 
     
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         let detailViewController:AiToyDetailViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("ToyDetail") as AiToyDetailViewController
         self.navigationController?.pushViewController(detailViewController, animated: true)
         NSLog("prepareForSegue!! sender is ")
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
 
 }
